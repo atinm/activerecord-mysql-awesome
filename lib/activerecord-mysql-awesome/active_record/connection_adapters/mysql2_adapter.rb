e8a48a392aaf2bb28f65acac7b909840bd1393a8
Robert Elwell <robert.elwell@lookout.com>
2016-11-17 16:03:47 -0800

    Enable JRuby support
    Requiring the mysql2 gem makes it impossible
    to run these useful patches without running mri.
    
    We would like to introduce this library to a JRuby
    project, and so the goal is to make it possible to
    skip including mysql2 as a dependency in cases where
    we can detect a Java runtime.

--- lib/activerecord-mysql-awesome/active_record/connection_adapters/mysql2_adapter.rb
+++ lib/activerecord-mysql-awesome/active_record/connection_adapters/mysql2_adapter.rb
@@ -1,26 +1,28 @@
-require 'active_record/connection_adapters/mysql2_adapter'
+unless RUBY_PLATFORM == 'java'
+  require 'active_record/connection_adapters/mysql2_adapter'
 
-module ActiveRecord
-  module ConnectionAdapters
-    class Mysql2Adapter < AbstractMysqlAdapter
-      class Version
-        include Comparable
+  module ActiveRecord
+    module ConnectionAdapters
+      class Mysql2Adapter < AbstractMysqlAdapter
+        class Version
+          include Comparable
 
-        def initialize(version_string)
-          @version = version_string.split('.').map(&:to_i)
+          def initialize(version_string)
+            @version = version_string.split('.').map(&:to_i)
+          end
+
+          def <=>(version_string)
+            @version <=> version_string.split('.').map(&:to_i)
+          end
+
+          def [](index)
+            @version[index]
+          end
         end
 
-        def <=>(version_string)
-          @version <=> version_string.split('.').map(&:to_i)
+        def version
+          @version ||= Version.new(@connection.server_info[:version].match(/^\d+\.\d+\.\d+/)[0])
         end
-
-        def [](index)
-          @version[index]
-        end
-      end
-
-      def version
-        @version ||= Version.new(@connection.server_info[:version].match(/^\d+\.\d+\.\d+/)[0])
       end
     end
   end
